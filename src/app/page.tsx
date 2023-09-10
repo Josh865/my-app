f358b182a3cf29c3b5439902b5f971dc7ab6c389
Josh White <joshdwhite@gmail.com>
2023-09-10 14:52:37 -0400

    Add fetch request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <main>Josh</main>;
+type PokemonResponse = {
+  name: string;
+  url: string;
+}[];
+
+async function fetchPokemon(): Promise<PokemonResponse> {
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon');
+  const data = await res.json();
+  return data.results;
+}
+
+export default async function Home() {
+  const pokemon = await fetchPokemon();
+
+  return (
+    <main className="container pt-6">
+      <h1 className="text-2xl font-medium tracking-tight">Pokemon</h1>
+      <ul className="mt-6">
+        {pokemon.map((pokemon) => (
+          <li key={pokemon.name}>{pokemon.name}</li>
+        ))}
+      </ul>
+    </main>
+  );
 }
