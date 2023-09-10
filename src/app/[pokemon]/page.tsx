be121745b0891a8966c81389116b9159cebaaab8
Josh White <joshdwhite@gmail.com>
2023-09-10 15:10:22 -0400

    Add layout, loading, and error

--- src/app/[pokemon]/page.tsx
+++ src/app/[pokemon]/page.tsx
@@ -1,5 +1,10 @@
 async function fetchPokemon(pokemon: string) {
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+  if (!res.ok) {
+    throw new Error(res.statusText);
+  }
+
   return res.json();
 }
 
@@ -13,10 +18,8 @@ export default async function Pokemon({ params }: PageArgs) {
   const pokemon = await fetchPokemon(params.pokemon);
 
   return (
-    <main className="container pt-6">
-      <h1 className="text-2xl font-medium tracking-tight">
-        {pokemon.name} - {pokemon.height}
-      </h1>
-    </main>
+    <h1 className="text-2xl font-medium tracking-tight">
+      {pokemon.name} - {pokemon.height}
+    </h1>
   );
 }
